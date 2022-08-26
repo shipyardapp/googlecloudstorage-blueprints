50612dd4721a4c3e244eaae84b5421c59e87d5d6
Shingai Shamu <mshamu95@gmail.com>
2022-08-26 17:43:57 +0200

    added remove file logic

---
+++ googlecloudstorage_blueprints/remove_file.py
@@ -0,0 +1,158 @@
+import os
+import json
+import argparse
+import re
+import tempfile
+import shipyard_utils as shipyard
+from google.cloud import storage
+from google.cloud.exceptions import *
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--bucket-name', dest='bucket_name', required=True)
+    parser.add_argument(
+        '--source-file-name-match-type',
+        dest='source_file_name_match_type',
+        default='exact_match',
+        choices={
+            'exact_match',
+            'regex_match'},
+        required=False)
+    parser.add_argument('--source-folder-name',
+                        dest='source_folder_name', default='', required=False)
+    parser.add_argument('--source-file-name',
+                        dest='source_file_name', required=True)
+    parser.add_argument(
+        '--service-account',
+        dest='gcp_application_credentials',
+        default=None,
+        required=True)
+    return parser.parse_args()
+
+
+def set_environment_variables(args):
+    """
+    Set GCP credentials as environment variables if they're provided via keyword
+    arguments rather than seeded as environment variables. This will override
+    system defaults.
+    """
+    credentials = args.gcp_application_credentials
+    try:
+        json_credentials = json.loads(credentials)
+        fd, path = tempfile.mkstemp()
+        print(f'Storing json credentials temporarily at {path}')
+        with os.fdopen(fd, 'w') as tmp:
+            tmp.write(credentials)
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = path
+        return path
+    except Exception:
+        print('Using specified json credentials file')
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials
+        return
+
+
+def find_google_cloud_storage_file_names(bucket, prefix=''):
+    """
+    Fetched all the files in the bucket which are returned in a list as
+    Google Blob objects
+    """
+    return list(bucket.list_blobs(prefix=prefix))
+
+
+
+def get_gclient(args):
+    """
+    Attempts to create the Google Cloud Storage Client with the associated
+    environment variables
+    """
+    try:
+        gclient = storage.Client()
+    except Exception as e:
+        print(f'Error accessing Google Cloud Storage with service account '
+              f'{args.gcp_application_credentials}')
+        raise(e)
+
+    return gclient
+
+
+def get_bucket(*,
+               gclient,
+               bucket_name):
+    """
+    Fetches and returns the bucket from Google Cloud Storage
+    """
+    try:
+        bucket = gclient.get_bucket(bucket_name)
+    except NotFound as e:
+        print(f'Bucket {bucket_name} does not exist\n {e}')
+        raise(e)
+
+    return bucket
+
+
+def get_storage_blob(bucket, source_folder_name, source_file_name):
+    """
+    Fetches and returns the single source file blob from the buck on
+    Google Cloud Storage
+    """
+    source_path = source_file_name
+    if source_folder_name != '':
+        source_path = f'{source_folder_name}/{source_file_name}'
+    blob = bucket.get_blob(source_path)
+    try:
+        blob.exists()
+        return blob
+    except Exception as e:
+        print(f'File {source_path} does not exist')
+        raise(e)
+
+
+
+def delete_google_cloud_storage_file(blob):
+    """
+    Deletes a selected file from Google cloud storage
+    """
+    blob_bucket, blob_name = blob.bucket.name, blob.name
+    blob.delete()
+    try: # check if deleted
+        blob.exists()
+        print("Error, blob not successfully deleted yet")
+    except Exception:
+        print(f"Blob {blob_bucket}/{blob_name} deleted.")
+
+
+
+def main():
+    args = get_args()
+    tmp_file = set_environment_variables(args)
+    bucket_name = args.bucket_name
+    source_file_name = args.source_file_name
+    source_folder_name = shipyard.files.clean_folder_name(args.source_folder_name)
+    source_file_name_match_type = args.source_file_name_match_type
+
+    gclient = get_gclient(args)
+    bucket = get_bucket(gclient=gclient, bucket_name=bucket_name)
+
+    if source_file_name_match_type == 'regex_match':
+        file_names = find_google_cloud_storage_file_names(
+            bucket=bucket, prefix=source_folder_name)
+        matching_file_names = shipyard.files.find_all_file_matches(file_names,
+                                                  re.compile(source_file_name))
+        print(f'{len(matching_file_names)} files found. Preparing to delete...')
+
+        for index, blob in enumerate(matching_file_names):
+            print(f'deleting file {index+1} of {len(matching_file_names)}')
+            delete_google_cloud_storage_file(blob=blob)
+    else:
+        blob = get_storage_blob(bucket=bucket,
+                                source_folder_name=source_folder_name,
+                                source_file_name=source_file_name)
+        delete_google_cloud_storage_file(blob=blob)
+    if tmp_file:
+        print(f'Removing temporary credentials file {tmp_file}')
+        os.remove(tmp_file)
+
+
+if __name__ == '__main__':
+    main()
